c0b158931d7dadf1a16ed733e5aea3dcba91d8ad
WeraGitHub <weronikalimberger@gmail.com>
2023-08-14 17:04:15 +0100

    Augmented Assignments

---
+++ week-1/augmented_assignments.py
@@ -0,0 +1,11 @@
+stein = 1
+pint = 1
+
+# print the value of the variable stein
+print(stein)
+
+# add stein and pint integer values
+stein += pint   # same as: stein = stein + pint
+
+# print the value of the variable stein
+print(stein)

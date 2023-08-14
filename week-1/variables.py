efcd093d02c72a55647f9e0c6525347e96d5a49a
WeraGitHub <weronikalimberger@gmail.com>
2023-08-14 17:05:17 +0100

    Concatenation and addition examples

---
+++ week-1/variables.py
@@ -0,0 +1,18 @@
+# Assign the integer with value 42 to the variable a
+a = 42
+# Assign the integer with value 9 to variable a
+b = 9
+
+# output a sum of two integer variables
+print(a + b)
+# show what type the variable a is (integer)
+print(type(a))
+
+# reassign the value of variable a from 42 to hello
+a = "Hello"
+# reassign the value of variable a from 42 to hello
+b = "World"
+# print the concatenated values of variables 'a' and 'b'
+print(a + b)
+# show what type the variable a (string)
+print(type(a))

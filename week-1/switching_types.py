69c988fd40e4ec6cf8ba05da4ea020186640d0b7
WeraGitHub <weronikalimberger@gmail.com>
2023-08-15 11:35:43 +0100

    Switching types and casting

---
+++ week-1/switching_types.py
@@ -0,0 +1,18 @@
+# Alex and Weronika
+
+# assign value of 42 to a variable named num
+num = 42
+# outputs a formatted string to the console with interpolated values
+print(f"Variable names num: {num}, which type is: {type(num)}")
+
+# assign value of 3.142 to a variable named pi
+pi = 3.142
+# assign value of 42/pi to our variable num
+num = 42/pi
+# prints a formatted string with interpolated values
+print(f"Variable names num: {num}, which type is: {type(num)}, because pi type is: {type(pi)}")
+
+# assign value of 5 to a variable named count
+port = 8081
+# print a concatenated string
+print("Unused port: " + str(port))  # converting count to string allows is to be printed

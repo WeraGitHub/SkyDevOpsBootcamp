44b6d32ffe9024a56633c53b15d4886a1ae4ab3f
WeraGitHub <weronikalimberger@gmail.com>
2023-08-24 11:15:13 +0100

    Simple Structured Flask App

--- week-3/structured-flask-app/application/routes.py
+++ week-3/structured-flask-app/application/routes.py
@@ -2,6 +2,22 @@ from flask import render_template
 from application import app
 
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def home():
     return render_template('home.html')
+
+
+@app.route('/hobbies/<firstname>', methods=['GET', 'POST'])
+def hobbies():
+    group_hobbies = ['Swimming, Gym, Football']
+    return render_template('home.html', name=firstname, hobbies_list=group_hobbies)
+
+
+@app.route('/about',methods=['GET'])
+def about():
+    return render_template('about.html', title="About")
+
+
+@app.route('/contact',methods=['GET'])
+def contact():
+    return render_template('contact.html', title="Contact!!!")
